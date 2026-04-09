e2443fe0f2e9c6e035d0dbd59dd10c893f9f4d62
malharjambhorkar <mmjambhorkar@gmail.com>
2026-04-09 20:37:57 +0530

    Updated db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   port:     parseInt(process.env.DB_PORT || '5432'),
   database: process.env.DB_NAME     || 'caz_db',
   user:     process.env.DB_USER     || 'postgres',
-  password: process.env.DB_PASSWORD || 'Yash@12345',
+  password: process.env.DB_PASSWORD ,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
